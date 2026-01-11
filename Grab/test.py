2404a4068a3b061a0a449116baf9d238b43b21d2
Rhinestone-Cowboy <diegopadilla005@gmail.com>
2026-01-11 03:42:37 -0800

    created new version of skate sensor code to allow for more convinient recording

--- Grab/test.py
+++ Grab/test.py
@@ -1,5 +1,4 @@
 import sk8
-# sk8.sk8_data_to_csv("Test.txt","stuff3.csv")
-# sk8.print_data("stuff3.csv", "acc_x")
+sk8.sk8_data_to_csv("test3.txt","stuff6.csv")
 
-sk8.print_all("stuff3.csv")
+sk8.print_all("stuff6.csv")
